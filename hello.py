bae22df969acc8489f0f77660023e0177a5eaa4f
Ming Qin <mingqin1@gmail.com>
2019-02-21 10:40:34 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,26 @@
-#!/usr/bin/python
+from datetime import timedelta, date, datetime
+import random
+import string
 import requests
-r = requests.get('https://api.github.com/events')
-print (r.content)
-print "Hello World!"
+import json
+
+start_date = datetime(2019, 2, 20)
+
+for x in range(60*1):
+    
+    single_date = start_date + timedelta(minutes=x)
+    dt = single_date.strftime("%Y-%m-%d")
+    doc = { "user" : "patriotism.com", 
+            "post_date" : single_date.isoformat(),  
+            "message" : ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12)) , 
+            "x": x,
+            "cpu": random.randint(0,100),
+            "io": random.randint(0,100)
+            
+    }
+    
+    print ( json.dumps(doc))
+
+#r = requests.get('https://api.github.com/events')
+#print(r.content)
+print("Hello, world!")
